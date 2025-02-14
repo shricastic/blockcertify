6435074c5d2a6bf71bb5d408232cdbf4e2322248
Shri <shricse07@gmail.com>
2025-02-14 10:47:33 +0530

    tests: retrive cert

--- anchor/tests/blockcertify.spec.ts
+++ anchor/tests/blockcertify.spec.ts
@@ -1,7 +1,7 @@
 import * as anchor from '@coral-xyz/anchor'
-import {Program} from '@coral-xyz/anchor'
 import {Keypair, PublicKey, SystemProgram} from '@solana/web3.js'
 import {Blockcertify} from '../target/types/blockcertify'
+import { web3, Program } from '@coral-xyz/anchor'
 
 describe('blockcertify', () => {
   const provider = anchor.AnchorProvider.env()
@@ -16,7 +16,8 @@ describe('blockcertify', () => {
   const program_name = "Diploma in AI Engineering";
   const institution_name = "Whatsapp University, India";
   const issued_date = "12/12/2025";
-  
+
+  const orgPublicKey = new web3.PublicKey("EpEV9oKB6fNXquh8h8Rb4Vx7kWmpakZCW9e1EU68An4d");
 
   let CertPDA: PublicKey;
   
@@ -56,6 +57,27 @@ describe('blockcertify', () => {
     expect(CertificateAccount.programName).toEqual(program_name);
     expect(CertificateAccount.institutionName).toEqual(institution_name);
     expect(CertificateAccount.issuedDate).toEqual(issued_date);
+  })
+
+  //::::::::::::::::::: Retrieve A Certificate ::::::::::::::::::::
+  it("Retrieve a certificate", async() => {
+
+    const [certPDA] = await web3.PublicKey.findProgramAddress(
+      [Buffer.from(recipient_id), orgPublicKey.toBuffer()],
+      program.programId
+    )
+
+    const CreatedCertificate = await program.account.certificate.fetch(certPDA);
+
+    console.log("::::::::::::::: Retrieve A Certificate :::::::::::::")
+    console.log(CreatedCertificate);
+
+
+    expect(CreatedCertificate.recipientId).toEqual(recipient_id);
+    expect(CreatedCertificate.recipientName).toEqual(recipient_name);
+    expect(CreatedCertificate.programName).toEqual(program_name);
+    expect(CreatedCertificate.institutionName).toEqual(institution_name);
+    expect(CreatedCertificate.issuedDate).toEqual(issued_date);
   })
 
   //::::::::::::::::::: Update A Certificate ::::::::::::::::::::
